47e96fc0844b95de0927967a851e87fdb7f04253
Sankyor <99845001+Sankyor@users.noreply.github.com>
2025-03-27 20:02:58 -0300

    Tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,17 @@
-
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app.js');
+
 
 describe('API Tests', () => {
-  it('should return a list of users', async () => {
-    const res = await request(app).get('/users');
+  it('should return a list of tasks', async () => {
+    const res = await request(app).get('/tasks');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveLength(2);
   });
 
-  it('should return a single user', async () => {
-    const res = await request(app).get('/users/1');
+  it('should return a single task', async () => {
+    const res = await request(app).get('/tasks/2');
     expect(res.statusCode).toEqual(200);
-    expect(res.body.name).toEqual('Alice');
+    expect(res.body.name).toEqual('Task 2');
   });
 });
