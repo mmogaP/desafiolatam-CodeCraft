f613513b1d8aa77f7bf0e21bb4c02a266dc92f96
Mauricio Moraga <mmoraga.m3@gmail.com>
2025-03-27 20:01:39 -0300

    feat:(test)

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,16 @@
+const request = require("supertest");
+const app = require("../app");
 
-const request = require('supertest');
-const app = require('../app');
-
-describe('API Tests', () => {
-  it('should return a list of users', async () => {
-    const res = await request(app).get('/users');
+describe("API Tests", () => {
+  it("should return a list of users", async () => {
+    const res = await request(app).get("/tasks");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveLength(2);
   });
 
-  it('should return a single user', async () => {
-    const res = await request(app).get('/users/1');
+  it("should return a single user", async () => {
+    const res = await request(app).get("/tasks/1");
     expect(res.statusCode).toEqual(200);
-    expect(res.body.name).toEqual('Alice');
+    expect(res.body.name).toEqual("Task 1");
   });
 });
